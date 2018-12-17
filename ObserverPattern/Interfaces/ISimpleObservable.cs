c96451a29f89c1e92ebf042d5b71beb695390a9c
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2018-12-17 13:29:40 +0100

    implement observable interface

--- ObserverPattern/Interfaces/ISimpleObservable.cs
+++ ObserverPattern/Interfaces/ISimpleObservable.cs
@@ -2,6 +2,8 @@ namespace ObserverPattern.Interfaces
 {
     public interface ISimpleObservable
     {
-        
+        void AddObserver(ISimpleObserver observer);
+        void RemoveObserver(ISimpleObserver observerToRemove);
+        void UpdateState();
     }
 }
